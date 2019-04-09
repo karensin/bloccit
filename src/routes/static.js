801cf53fb4c2a3ee14e6a25c5cc5e4a188475c79
Karen k  sin <karenksin@Karens-MacBook-Air.local>
2019-04-08 18:15:13 -0700

    intro to express

---
+++ src/routes/static.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res, next) => {
+  res.send("Welcome to Bloccit");
+});
+
+
+router.get("/marco", (req, res, next) => {
+  res.send("Polo");
+});
+
+
+module.exports = router;
