b75ac0e54f8067be85b5c00c1a15f44c8b2c8510
Karen k  sin <karenksin@Karens-MacBook-Air.local>
2019-04-09 18:21:32 -0700

    second

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -5,22 +5,13 @@ const marcoBase= "http://localhost:3000/marco";
 
 
 describe("routes : static", () => {
-
- describe("GET /", () => {
-   it("should return status code 200", (done) => {
-
-     request.get(base, (err, res, body) => {
-       expect(res.statusCode).toBe(200);
-       done();
-     });
-   });
-
-
-it("should return string 'Polo'",(done)=>{
-  request.get(marcoBase,(err,res,body)=>{
-    expect(body).toContain('Polo');
-    done();
-});
+  describe("GET /", () => {
+    it("should return status code 200 and have 'Welcome to Bloccit' in the body of the response", () => {
+      request.get(base, (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain("Welcome to Bloccit");
+        done();
+      });
     });
   });
 });
