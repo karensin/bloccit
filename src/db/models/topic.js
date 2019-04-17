9e7d06bb890bd6879e2d0c041f272d30111d216e
Karen k  sin <karenksin@Karens-MacBook-Air.local>
2019-04-16 18:11:57 -0700

    test

---
+++ src/db/models/topic.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var Topic = sequelize.define('Topic', {
+    title: DataTypes.STRING,
+    description: DataTypes.STRING
+  }, {});
+  Topic.associate = function(models) {
+    Topic.hasMany(models.Banner, {
+      foreignKey: "topicId",
+      as: "banners",
+    });
+  // associations can be defined here
+  };
+  return Topic;
+};
