770692e426e312f876af15dcdf8ebc8c0f54da01
Karen k  sin <karenksin@Karens-MacBook-Air.local>
2019-04-25 16:20:14 -0700

    post-resource-2 assignment completed and tested

---
+++ src/db/migrations/20190425224743-create-flair.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Flairs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      color: {
+       allowNull: false,
+       type: Sequelize.STRING
+     },
+      body: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      postId: {
+             type: Sequelize.INTEGER,
+             allowNull: false,
+             onDelete: "CASCADE",
+     	references: {
+     	  model: "Posts",
+     	  key: "id",
+     	  as: "postId"
+     	  },
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Flairs');
+  }
+};
--- src/db/models/post.js
+++ src/db/models/post.js
@@ -21,6 +21,10 @@ module.exports = (sequelize, DataTypes) => {
       foreignKey: "topicId",
       onDelete: "CASCADE"
     });
+    Post.hasMany(models.Flair, {
+      foreignKey: "postId",
+      as: "flairs"
+    });
   };
   return Post;
 };
