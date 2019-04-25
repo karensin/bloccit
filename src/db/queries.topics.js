651ba0d8ca0b39674875f86b7905a075603f85b2
Karen k  sin <karenksin@Karens-MacBook-Air.local>
2019-04-24 21:04:34 -0700

    checkpoint post resource-2

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -1,12 +1,11 @@
 const Topic = require("./models").Topic;
+const Post = require("./models").Post;
 
 module.exports = {
 
-//#1
   getAllTopics(callback){
     return Topic.all()
 
-//#2
     .then((topics) => {
       callback(null, topics);
     })
@@ -14,58 +13,62 @@ module.exports = {
       callback(err);
     })
   },
-  getTopic(id, callback){
-    return Topic.findById(id)
-    .then((topic) => {
-      callback(null, topic);
-    })
-    .catch((err) => {
-      callback(err);
-    })
-  },
-  addTopic(newTopic, callback){
-       return Topic.create({
-         title: newTopic.title,
-         description: newTopic.description
-       })
-       .then((topic) => {
-         callback(null, topic);
-       })
-       .catch((err) => {
-         callback(err);
-       })
-     },
-  deleteTopic(id, callback){
-    return Topic.destroy({
-      where: {id}
-    })
-    .then((topic) => {
-      callback(null, topic);
-    })
-    .catch((err) => {
-      callback(err);
-    })
-  },
-  updateTopic(id, updatedTopic, callback){
-   return Topic.findById(id)
-   .then((topic) => {
-     if(!topic){
-       return callback("Topic not found");
-     }
 
-//#1
-     topic.update(updatedTopic, {
-       fields: Object.keys(updatedTopic)
-     })
-     .then(() => {
+  getTopic(id, callback){
+     return Topic.findById(id, {
+	include: [{
+        model: Post,
+        as: "posts"
+      }]
+    })
+     .then((topic) => {
        callback(null, topic);
      })
      .catch((err) => {
        callback(err);
+     })
+   },
+
+  addTopic(newTopic, callback){
+      return Topic.create({
+        title: newTopic.title,
+        description: newTopic.description
+      })
+      .then((topic) => {
+        callback(null, topic);
+      })
+      .catch((err) => {
+        callback(err);
+      })
+  },
+
+  updateTopic(id, updatedTopic, callback){
+      return Topic.findById(id)
+      .then((topic) => {
+	if(!topic){
+	  return callback("Topic not found");
+      }
+        topic.update(updatedTopic, {
+          fields: Object.keys(updatedTopic)
+        })
+        .then(() => {
+          callback(null, topic);
+        })
+        .catch((err) => {
+          callback(err);
+        });
      });
-   });
- },
+  },
 
-
-
+  deleteTopic(id, callback){
+     return Topic.destroy({
+       where: {id}
+     })
+     .then((topic) => {
+       callback(null, topic);
+     })
+     .catch((err) => {
+       callback(err);
+     })
+   }
 }
